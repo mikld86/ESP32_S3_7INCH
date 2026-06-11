779414bd30b5e7f2962d77c69cf7eb4609f4d351
mikld86 <llerudleahcim@yahoo.co.uk>
2026-06-11 16:28:30 +0800

    hgacking in own lvgl 8 ui and VictronBLE Stack

--- src/main.cpp
+++ src/main.cpp
@@ -1,68 +1,17 @@
-
 /*******************************************************************************
- * LVGL Widgets
- * This is a widgets demo for LVGL - Light and Versatile Graphics Library
- * import from: https://github.com/lvgl/lv_demos.git
- *
- * Dependent libraries:
- * LVGL: https://github.com/lvgl/lvgl.git
-
- * Touch libraries:
- * FT6X36: https://github.com/strange-v/FT6X36.git
- * GT911: https://github.com/TAMCTec/gt911-arduino.git
- * XPT2046: https://github.com/PaulStoffregen/XPT2046_Touchscreen.git
- *
- * LVGL Configuration file:
- * Copy your_arduino_path/libraries/lvgl/lv_conf_template.h
- * to your_arduino_path/libraries/lv_conf.h
- * Then find and set:
- * #define LV_COLOR_DEPTH     16
- * #define LV_TICK_CUSTOM     1
- *
- * For SPI display set color swap can be faster, parallel screen don't set!
- * #define LV_COLOR_16_SWAP   1
- *
- * Optional: Show CPU usage and FPS count
- * #define LV_USE_PERF_MONITOR 1
+ * LVGL 7-Inch Waveshare Victron BLE Integrated Dashboard
+ * Preconfigured for Arduino_GFX RGB panel & Standalone Bluetooth Decryption
  ******************************************************************************/
-//#include "lv_demo_widgets.h"
 #include <lvgl.h>
-#include <demos/lv_demos.h>
-/*******************************************************************************
- * Start of Arduino_GFX setting
- *
- * Arduino_GFX try to find the settings depends on selected board in Arduino IDE
- * Or you can define the display dev kit not in the board list
- * Defalult pin list for non display dev kit:
- * Arduino Nano, Micro and more: CS:  9, DC:  8, RST:  7, BL:  6, SCK: 13, MOSI: 11, MISO: 12
- * ESP32 various dev board     : CS:  5, DC: 27, RST: 33, BL: 22, SCK: 18, MOSI: 23, MISO: nil
- * ESP32-C3 various dev board  : CS:  7, DC:  2, RST:  1, BL:  3, SCK:  4, MOSI:  6, MISO: nil
- * ESP32-S2 various dev board  : CS: 34, DC: 35, RST: 33, BL: 21, SCK: 36, MOSI: 35, MISO: nil
- * ESP32-S3 various dev board  : CS: 40, DC: 41, RST: 42, BL: 48, SCK: 36, MOSI: 35, MISO: nil
- * ESP8266 various dev board   : CS: 15, DC:  4, RST:  2, BL:  5, SCK: 14, MOSI: 13, MISO: 12
- * Raspberry Pi Pico dev board : CS: 17, DC: 27, RST: 26, BL: 28, SCK: 18, MOSI: 19, MISO: 16
- * RTL8720 BW16 old patch core : CS: 18, DC: 17, RST:  2, BL: 23, SCK: 19, MOSI: 21, MISO: 20
- * RTL8720_BW16 Official core  : CS:  9, DC:  8, RST:  6, BL:  3, SCK: 10, MOSI: 12, MISO: 11
- * RTL8722 dev board           : CS: 18, DC: 17, RST: 22, BL: 23, SCK: 13, MOSI: 11, MISO: 12
- * RTL8722_mini dev board      : CS: 12, DC: 14, RST: 15, BL: 13, SCK: 11, MOSI:  9, MISO: 10
- * Seeeduino XIAO dev board    : CS:  3, DC:  2, RST:  1, BL:  0, SCK:  8, MOSI: 10, MISO:  9
- * Teensy 4.1 dev board        : CS: 39, DC: 41, RST: 40, BL: 22, SCK: 13, MOSI: 11, MISO: 12
- ******************************************************************************/
 #include <Arduino_GFX_Library.h>
-#define TFT_BL 2
-//#define GFX_BL DF_GFX_BL // default backlight pin, you may replace DF_GFX_BL to actual backlight pin
+#include <VictronBLE.h> // Standalone BLE library
+#include "touch.h"
 
-/* More dev device declaration: https://github.com/moononournation/Arduino_GFX/wiki/Dev-Device-Declaration */
+#define TFT_BL 2
+
 #if defined(DISPLAY_DEV_KIT)
 Arduino_GFX *gfx = create_default_Arduino_GFX();
-#else /* !defined(DISPLAY_DEV_KIT) */
-
-/* More data bus class: https://github.com/moononournation/Arduino_GFX/wiki/Data-Bus-Class */
-//Arduino_DataBus *bus = create_default_Arduino_DataBus();
-
-/* More display class: https://github.com/moononournation/Arduino_GFX/wiki/Display-Class */
-//Arduino_GFX *gfx = new Arduino_ILI9341(bus, DF_GFX_RST, 0 /* rotation */, false /* IPS */);
-
+#else 
 
 Arduino_ESP32RGBPanel *bus = new Arduino_ESP32RGBPanel(
     GFX_NOT_DEFINED /* CS */, GFX_NOT_DEFINED /* SCK */, GFX_NOT_DEFINED /* SDA */,
@@ -71,34 +20,51 @@ Arduino_ESP32RGBPanel *bus = new Arduino_ESP32RGBPanel(
     9 /* G0 */, 46 /* G1 */, 3 /* G2 */, 8 /* G3 */, 16 /* G4 */, 1 /* G5 */,
     15 /* B0 */, 7 /* B1 */, 6 /* B2 */, 5 /* B3 */, 4 /* B4 */
 );
-// option 1:
-// 7寸 50PIN 800*480
-Arduino_RPi_DPI_RGBPanel *gfx = new Arduino_RPi_DPI_RGBPanel(
-  bus,
-//  800 /* width */, 0 /* hsync_polarity */, 8/* hsync_front_porch */, 2 /* hsync_pulse_width */, 43/* hsync_back_porch */,
-//  480 /* height */, 0 /* vsync_polarity */, 8 /* vsync_front_porch */, 2/* vsync_pulse_width */, 12 /* vsync_back_porch */,
-//  1 /* pclk_active_neg */, 16000000 /* prefer_speed */, true /* auto_flush */);
 
+Arduino_RPi_DPI_RGBPanel *gfx = new Arduino_RPi_DPI_RGBPanel(
+    bus,
     800 /* width */, 0 /* hsync_polarity */, 210 /* hsync_front_porch */, 30 /* hsync_pulse_width */, 16 /* hsync_back_porch */,
     480 /* height */, 0 /* vsync_polarity */, 22 /* vsync_front_porch */, 13 /* vsync_pulse_width */, 10 /* vsync_back_porch */,
     1 /* pclk_active_neg */, 16000000 /* prefer_speed */, true /* auto_flush */);
+#endif 
 
-#endif /* !defined(DISPLAY_DEV_KIT) */
-/*******************************************************************************
- * End of Arduino_GFX setting
- ******************************************************************************/
-
-/*******************************************************************************
- * Please config the touch panel in touch.h
- ******************************************************************************/
-#include "touch.h"
-
-/* Change to your screen resolution */
+/* LVGL Engine Globals */
 static uint32_t screenWidth;
 static uint32_t screenHeight;
 static lv_disp_draw_buf_t draw_buf;
 static lv_color_t *disp_draw_buf;
 static lv_disp_drv_t disp_drv;
+
+/* --- VICTRON INTEGRATION ENGINE --- */
+VictronBLE victron;
+
+// Structure to pass data safely across processor cores
+struct VictronSharedState {
+    float voltage;
+    float current;
+    float soc;
+    float power;
+    bool dataReady;
+};
+volatile VictronSharedState sharedMetrics = {0.0f, 0.0f, 0.0f, 0.0f, false};
+
+// Temporary placeholder pointers for verifying the BLE incoming data stream
+lv_obj_t *lbl_battery;
+lv_obj_t *lbl_solar;
+
+// Background Core 0 Callback: Executes when a matching encrypted payload is captured
+void onVictronBleData(const VictronDevice* device) {
+    if (device->getType() == VICTRON_SMART_SHUNT) {
+        sharedMetrics.voltage   = device->getBatteryVoltage();
+        sharedMetrics.current   = device->getBatteryCurrent();
+        sharedMetrics.soc       = device->getSOC();
+        sharedMetrics.dataReady = true;
+    } else if (device->getType() == VICTRON_MPPT) {
+        sharedMetrics.power     = device->getSolarPower();
+        sharedMetrics.dataReady = true;
+    }
+}
+/* --------------------------------- */
 
 /* Display flushing */
 void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
@@ -115,6 +81,7 @@ void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color
   lv_disp_flush_ready(disp);
 }
 
+/* Touch Input driver */
 void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
 {
   if (touch_has_signal())
@@ -122,14 +89,8 @@ void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
     if (touch_touched())
     {
       data->state = LV_INDEV_STATE_PR;
-
-      /*Set the coordinates*/
       data->point.x = touch_last_x;
       data->point.y = touch_last_y;
-      Serial.print( "Data x " );
-      Serial.println( data->point.x );
-      Serial.print( "Data y " );
-      Serial.println( data->point.y );
     }
     else if (touch_released())
     {
@@ -144,14 +105,9 @@ void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
 
 void setup()
 {
-  //gfx->getFramebuffer;
-  //gfx->drawBitmap;
-  //gfx->drawPixel;
-  
   Serial.begin(115200);
-  // while (!Serial);
-  Serial.println("LVGL Widgets Demo");
-  // Init Display
+  Serial.println("Victron 7-Inch Standalone Screen Init...");
+
   gfx->begin();
 #ifdef TFT_BL
   pinMode(TFT_BL, OUTPUT);
@@ -159,67 +115,102 @@ void setup()
 
   ledcSetup(0, 300, 8);
   ledcAttachPin(TFT_BL, 0);
-  ledcWrite(0, 255); /* Screen brightness can be modified by adjusting this parameter. (0-255) */
-  
+  ledcWrite(0, 255); 
 #endif
-  gfx->fillScreen(RED);
-  delay(500);
-  gfx->fillScreen(GREEN);
-  delay(500);
-  gfx->fillScreen(BLUE);
-  delay(500);
+
+  // Clear splash screens quickly
   gfx->fillScreen(BLACK);
-  delay(500);
+  
   lv_init();
 
-    // Init touch device
+  // Init touch device
   pinMode(TOUCH_GT911_RST, OUTPUT);
   digitalWrite(TOUCH_GT911_RST, LOW);
   delay(10);
   digitalWrite(TOUCH_GT911_RST, HIGH);
   delay(10);
   touch_init();
-//  touch.setTouch( calData );
 
   screenWidth = gfx->width();
   screenHeight = gfx->height();
+
 #ifdef ESP32
-  disp_draw_buf = (lv_color_t *)heap_caps_malloc(sizeof(lv_color_t) * screenWidth *screenHeight/4  , MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
+  disp_draw_buf = (lv_color_t *)heap_caps_malloc(sizeof(lv_color_t) * screenWidth * screenHeight / 4, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
 #else
-  disp_draw_buf = (lv_color_t *)malloc(sizeof(lv_color_t) * screenWidth *screenHeight/4);
+  disp_draw_buf = (lv_color_t *)malloc(sizeof(lv_color_t) * screenWidth * screenHeight / 4);
 #endif
+
   if (!disp_draw_buf)
   {
-    Serial.println("LVGL disp_draw_buf allocate failed!");
+    Serial.println("LVGL Buffer Allocation Failed!");
+    while(1) delay(100);
   }
-  else
-  {
-    lv_disp_draw_buf_init(&draw_buf, disp_draw_buf, NULL, screenWidth *screenHeight/4);
 
-    /* Initialize the display */
-    lv_disp_drv_init(&disp_drv);
-    /* Change the following line to your display resolution */
-    disp_drv.hor_res = screenWidth;
-    disp_drv.ver_res = screenHeight;
-    disp_drv.flush_cb = my_disp_flush;
-    disp_drv.draw_buf = &draw_buf;
-    lv_disp_drv_register(&disp_drv);
+  lv_disp_draw_buf_init(&draw_buf, disp_draw_buf, NULL, screenWidth * screenHeight / 4);
 
-    /* Initialize the (dummy) input device driver */
-    static lv_indev_drv_t indev_drv;
-    lv_indev_drv_init(&indev_drv);
-    indev_drv.type = LV_INDEV_TYPE_POINTER;
-    indev_drv.read_cb = my_touchpad_read;
-    lv_indev_drv_register(&indev_drv);
+  /* Setup Display Driver */
+  lv_disp_drv_init(&disp_drv);
+  disp_drv.hor_res = screenWidth;
+  disp_drv.ver_res = screenHeight;
+  disp_drv.flush_cb = my_disp_flush;
+  disp_drv.draw_buf = &draw_buf;
+  lv_disp_drv_register(&disp_drv);
 
-    lv_demo_widgets();
-    //lv_demo_music(); 
-    Serial.println("Setup done");
-  }
+  /* Setup Input Touch Driver */
+  static lv_indev_drv_t indev_drv;
+  lv_indev_drv_init(&indev_drv);
+  indev_drv.type = LV_INDEV_TYPE_POINTER;
+  indev_drv.read_cb = my_touchpad_read;
+  lv_indev_drv_register(&indev_drv);
+
+  /* --- HACK IN YOUR CUSTOM UI / FILES HERE --- */
+  // If using an exported SquareLine studio framework, comment out the lines below and call:
+  // ui_init(); 
+  
+  // Temporary placeholders for validation:
+  lbl_battery = lv_label_create(lv_scr_act());
+  lv_obj_set_style_text_font(lbl_battery, &lv_font_montserrat_28, 0);
+  lv_obj_align(lbl_battery, LV_ALIGN_CENTER, 0, -50);
+  lv_label_set_text(lbl_battery, "Waiting for SmartShunt BLE...");
+
+  lbl_solar = lv_label_create(lv_scr_act());
+  lv_obj_set_style_text_font(lbl_solar, &lv_font_montserrat_28, 0);
+  lv_obj_align(lbl_solar, LV_ALIGN_CENTER, 0, 50);
+  lv_label_set_text(lbl_solar, "Waiting for MPPT BLE...");
+  /* ------------------------------------------- */
+
+  /* --- INITIALIZE VICTRON DECRYPTION --- */
+  // Replace with your real Victron MAC addresses and extracted 32-character Hex AES keys
+  victron.addDevice("SmartShunt", "aa:bb:cc:dd:ee:ff", "00112233445566778899aabbccddeeff");
+  victron.addDevice("SmartMPPT",  "11:22:33:44:55:66", "ffeeddccbbaa99887766554433221100");
+  
+  victron.setCallback(onVictronBleData);
+  victron.begin(); // Fires up background parsing tasks asynchronously on Core 0
+  
+  Serial.println("System Engine Live.");
 }
 
 void loop()
 {
-  lv_timer_handler(); /* let the GUI do its work */
+  lv_timer_handler(); /* Let the GUI engine refresh objects */
+  
+  // Thread-safely pipe the background data metrics to your active screen widgets
+  static uint32_t lastWidgetRefresh = 0;
+  if (millis() - lastWidgetRefresh > 1000) {
+      if (sharedMetrics.dataReady) {
+          
+          // --- HACK IN YOUR LVGL LABELS/METERS DATA RETRIEVAL HERE ---
+          // Replace these placeholder calls with your custom UI setters (e.g. lv_bar_set_value)
+          lv_label_set_text_fmt(lbl_battery, "Battery: %.2fV  |  %.2fA  |  %.1f%%", 
+                                sharedMetrics.voltage, sharedMetrics.current, sharedMetrics.soc);
+                                
+          lv_label_set_text_fmt(lbl_solar, "Solar Production: %.0f Watts", 
+                                sharedMetrics.power);
+          
+          sharedMetrics.dataReady = false; // reset transaction flag
+      }
+      lastWidgetRefresh = millis();
+  }
+  
   delay(5);
 }
